51b885d7ed7f5c84dc87937f6f7611ce28ca4dcc
Andy Heard <a.heard@cmcmarkets.com>
2025-02-19 09:59:48 +0000

    Documentation

--- Sources/PartialUpdate/PartialUpdateMacros.swift
+++ Sources/PartialUpdate/PartialUpdateMacros.swift
@@ -1,6 +1,14 @@
 
+/// Generates a default implementation of the ``PartiallyUpdatable`` protocol for a `struct` or `enum` type.
+///
+/// This macro will synthesize an implemenation for ``PartiallyUpdatable/PartialUpdate``, ``PartiallyUpdatable/update(from:)`` and ``PartiallyUpdatable/updated(with:)``. This implementation assumes that a memberwise initializer is available  for the type the macro is attached to.
+///
+/// The macro will make the synthesized ``PartiallyUpdatable/PartialUpdate`` type shadow the `Codable`, `Encodable` and `Decodable` inheritances of the parent type.
 @attached(extension, conformances: PartiallyUpdatable, Codable, Encodable, Decodable, names: named(PartialUpdate), named(update(from: )), named(updated(with: )))
 public macro PartiallyUpdatable() = #externalMacro(module: "PartialUpdateMacros", type: "PartiallyUpdatableMacro")
 
+/// Instructs the `@PartiallyUpdatable` macro to ignore the property that this macro is attached to.
+///
+/// This can only be applied to properties on a `struct`, and that property will not have a synthesized property in the `PartialUpdate` type. Additionally, the ``PartiallyUpdatable/updated(with:)`` function will instead just inject the current value of this property in the initializer without attempting to update it.
 @attached(accessor, names: named(didSet))
 public macro PartiallyUpdatableIgnored() = #externalMacro(module: "PartialUpdateMacros", type: "PartiallyUpdatableIgnoredMacro")
--- Sources/PartialUpdate/PartiallyUpdatable.swift
+++ Sources/PartialUpdate/PartiallyUpdatable.swift
@@ -1,16 +1,33 @@
 
+/// Indicates that a type can create an object mirroring itself, but only containing differences from some other instance of the same type, and can apply that difference object to update itself with those differences.
 public protocol PartiallyUpdatable: Hashable {
 
     // MARK: - Associated type
+
+    /// A type containing the differences between two instances of the same type.
     associatedtype PartialUpdate
 
     // MARK: - Functions
+
+    /// Updates this instance with the provided `PartialUpdate` object.
+    /// - Parameter partialUpdate: The object containing the changes to be applied to this instance. If `nil` then no changes are made.
+    /// - Returns: The updated instance of the object.
     func updated(with partialUpdate: PartialUpdate?) throws -> Self
+
+    /// Creates a `PartialUpdate` object instance containing all the changes between this value, and the provided `oldValue`. The difference object should contain values on the calling instance only if it is not equal to that on the `oldValue`.
+    /// - Parameter oldValue: The instance to compare values to and return only values that aren't equal. Equal values should be represented by `nil` to indicate no change.
+    /// - Returns: The object containing the differences between this instance and the `oldValue`. If the two instances are equal, then `nil` is returned.
     func update(from oldValue: Self) -> PartialUpdate?
 }
 
 // MARK: - Error
+
+/// Errors that can occur when applying a `PartialUpdate` instance to a `PartiallyUpdatable` instance.
 public enum PartialUpdateError: Error {
+
+    /// A `nil` optional value was attempted to be set to non-nil with only a partial set of data.
     case updatingNilWithPartialValue
+
+    /// An enum case was attempted to be set to another case with a partial set off associated value data.
     case updatingEnumWithIncorrectCase
 }
